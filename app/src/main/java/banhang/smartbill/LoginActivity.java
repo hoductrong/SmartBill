5f7cc3413c696741e17b28f2fec6efdd45cd7b5f
phuc-kun <www.narutocntt@gmail.com>
2017-11-25 14:56:24 +0700

    add main_screen.xml,hoadonAdapter,Hoadon,main-item(custom),login.xml, xai extends appcompatactivity thay cho activity de inflate cai button dong

--- app/src/main/java/banhang/smartbill/LoginActivity.java
+++ app/src/main/java/banhang/smartbill/LoginActivity.java
@@ -1,13 +1,13 @@
 package banhang.smartbill;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class LoginActivity extends Activity {
-
+public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
     }
 }
