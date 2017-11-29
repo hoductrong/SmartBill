cacaa8da5f89b60dc41f43bf4f94f96a9d3180cc
trong.ho <trong.hoduc@gmail.com>
2017-11-29 15:05:48 +0700

    commit APIToken

--- app/src/main/java/banhang/smartbill/DAL/TokenAPI.java
+++ app/src/main/java/banhang/smartbill/DAL/TokenAPI.java
@@ -1,5 +1,7 @@
 package banhang.smartbill.DAL;
 
+import android.widget.SimpleCursorTreeAdapter;
+
 import java.net.HttpURLConnection;
 import java.net.ProtocolException;
 
@@ -12,14 +14,14 @@ import banhang.smartbill.Entity.GrantTokenResult;
 public class TokenAPI {
     public static String TOKEN = null;
 
-    public String getToken(){
+    public String getToken(String username, String password){
         BaseAPI tool = new BaseAPI("http://quanlibanhang.azurewebsites.net/api/token");
         HttpURLConnection conn = tool.getConnection();
         try{
             conn.setRequestMethod("POST");
             conn.setRequestProperty("Content-Type","application/x-www-form-urlencoded");
             conn.setRequestProperty("Accept","application/json");
-            String content = "grant_type=password&username=dinhhongphi&password=phi123";
+            String content = "grant_type=password&username="+username+"&password="+password;
             GrantTokenResult result = tool.getResult(conn,content,GrantTokenResult.class);
             TOKEN = result.getAccess_token();
             return result.getAccess_token();
