b740fb07333ccf58312e9d3fe37faa057cd22a4c
Đinh Hồng Phi <dinhhongphi@gmail.com>
2017-12-05 21:38:09 +0700

    show customer name when add order

---
+++ app/src/main/java/banhang/smartbill/Utility/CreateOrderDialog.java
@@ -0,0 +1,104 @@
+package banhang.smartbill.Utility;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Message;
+import android.support.annotation.NonNull;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.os.Handler;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import banhang.smartbill.Activity.MainActivity;
+import banhang.smartbill.DAL.CustomerAPI;
+import banhang.smartbill.Entity.Customer;
+import banhang.smartbill.Entity.UnauthorizedAccessException;
+import banhang.smartbill.Fragment.OrderFragment;
+import banhang.smartbill.R;
+
+/**
+ * Created by KARATA on 05/12/2017.
+ */
+
+public class CreateOrderDialog extends Dialog {
+    private RelativeLayout btn_close;
+    private Button btn_create_order;
+    private AutoCompleteTextView tv_customer;
+
+    public CreateOrderDialog(@NonNull Context context) {
+        super(context);
+    }
+
+    @Override
+    public void setContentView(int layoutResID) {
+        super.setContentView(layoutResID);
+        btn_close = (RelativeLayout) findViewById(R.id.btn_close);
+        btn_create_order = (Button)findViewById(R.id.btn_create_order);
+        tv_customer = (AutoCompleteTextView)findViewById(R.id.tv_customer);
+        //get customer list
+        getCustomerList();
+    }
+
+    private void getCustomerList(){
+        final Handler handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                switch (msg.what){
+                    case 1: //get success
+                        //set data for autoComplete textView
+                        ///set adapter for autocomplete
+                        ArrayList<Customer> data = (ArrayList<Customer>) msg.obj;
+                        CustomerListAdapter adapter= new CustomerListAdapter(getContext(),
+                                R.layout.customer_name_list_item,data);
+                        tv_customer.setAdapter(adapter);
+                        break;
+                    case 2: //unauthorize
+                        Toast.makeText(getContext(),R.string.unauthorize,Toast.LENGTH_LONG).show();
+                        MainActivity.requireLogin(getContext());
+                        break;
+                }
+            }
+        };
+
+        Thread getCustomerThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                CustomerAPI api = new CustomerAPI();
+                try{
+                    List<Customer> customerList = api.getCustomers();
+                    Message message = handler.obtainMessage(1,customerList);
+                    handler.sendMessage(message);
+                }catch(UnauthorizedAccessException ex){
+                    Message message = handler.obtainMessage(2,"Unauthorize");
+                    handler.sendMessage(message);
+                }
+            }
+        });
+        getCustomerThread.start();
+    }
+
+    //handle action when close button is cliked
+    public void setOnCloseButtonClickListener(View.OnClickListener listener){
+        if(listener != null)
+            btn_close.setOnClickListener(listener);
+        else
+            throw new NullPointerException();
+    }
+
+    //handle action when create order clicked
+    public void setOnCreateOrderClickListener(View.OnClickListener listener){
+        if(listener != null){
+            btn_create_order.setOnClickListener(listener);
+        }else
+            throw new NullPointerException();
+    }
+}
---
+++ app/src/main/java/banhang/smartbill/Utility/CustomerListAdapter.java
@@ -0,0 +1,109 @@
+package banhang.smartbill.Utility;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Filter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import banhang.smartbill.Entity.Customer;
+import banhang.smartbill.R;
+
+/**
+ * Created by KARATA on 05/12/2017.
+ * Hiển thị danh sách khách hàng để tạo hóa đơn
+ */
+
+public class CustomerListAdapter extends ArrayAdapter<Customer> {
+    private List<Customer> customerList;
+    private List<Customer> filterList;
+    private int layout;
+    public CustomerListAdapter(@NonNull Context context, int resource,List<Customer> customerList) {
+        super(context,resource);
+        this.customerList = customerList;
+        this.layout = resource;
+        filterList = new ArrayList<>();
+    }
+
+    @Override
+    public int getCount() {
+        return filterList.size();
+    }
+
+    @Nullable
+    @Override
+    public Customer getItem(int position) {
+        Customer customer = filterList.get(position);
+        return customer;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @NonNull
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            public CharSequence convertResultToString(Object resultValue) {
+                return ((Customer)resultValue).getName();
+            }
+            //lọc các item thỏa mãn điều kiện
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                FilterResults filterResults = new FilterResults();
+                List<Customer> customerSuggestion = new ArrayList<>();
+                if(charSequence != null){
+                    for(Customer c : customerList){
+                        if(c.getName().toLowerCase().
+                                startsWith(charSequence.toString().toLowerCase())){
+                            customerSuggestion.add(c);
+                        }
+                    }
+                    filterResults.count = customerSuggestion.size();
+                    filterResults.values = customerSuggestion;
+                }
+                return filterResults;
+            }
+            //sau khi lọc xong sẽ tạo ra một danh sách list item để được hiển thị
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                filterList.clear();
+                if(filterResults != null && filterResults.count > 0){
+                    for(Object o : (List<?>)filterResults.values){
+                        if(o instanceof Customer){
+                            filterList.add((Customer)o);
+                        }
+                    }
+                    notifyDataSetChanged();
+                }else if(filterResults == null){
+                    filterList.addAll(customerList);
+                    notifyDataSetChanged();
+                }
+            }
+        };
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if(convertView == null){
+            LayoutInflater inflater = (LayoutInflater) getContext().
+                    getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(this.layout,null);
+        }
+        TextView tv_customer_name = convertView.findViewById(R.id.tv_customer_name);
+        tv_customer_name.setText(getItem(position).getName());
+        return convertView;
+    }
+}
